05bcc881997d56afbc7135ff89d7f7df696d2170
Varun Ramani <varun.ramani@gmail.com>
2019-06-17 22:15:25 -0400

    Added js navbar transition

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+particlesJS.load("hero", "js/particles.json");
+
+var navbar = document.getElementById("page-navbar");
+function updateNavbar() {
+    console.log("Updating");
+    if (window.scrollY >= window.innerHeight) {
+        navbar.style.backgroundColor = 'rgba(0,0,0,0.9)';
+    } else {
+        navbar.style.backgroundColor = `rgba(0,0,0,${0.95 * (window.scrollY / window.innerHeight)})`;
+    }
+    setTimeout(updateNavbar, 30);
+}
+updateNavbar();
