035b14fd7a798614b94376212b6620b155fb86ba
Varun Ramani <varun.ramani@gmail.com>
2019-08-13 22:26:15 -0400

    Added new sponsors

--- js/main.js
+++ js/main.js
@@ -15,11 +15,14 @@ document.getElementById("hero").style.paddingTop = `${navbar.offsetHeight}px`;
 
 $('#page-navbar').bind('click', 'ul li a', function(event) {
     event.preventDefault();
-    console.log("Scrolling");
     var location = $(event.target.hash).offset().top;
     $('html, body').animate({
         scrollTop: location
     }, 500);
 });
 
+function visitLink(link) {
+    console.log("Visiting " + link);
+    location.href=link;
+}
 // updateNavbar();
